71f0f7700d98b412f2f8f0a9c8010ad9debfd47a
Onurcan Kurt <onurcankurt96@gmail.com>
2024-08-19 14:01:33 +0300

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by onur on 19.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
